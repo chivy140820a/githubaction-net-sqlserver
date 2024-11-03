4fd87d6c7cbfa138c6fc7f16d68f5348279820e2
chivy140820a <chivy1408@gmail.com>
2024-11-03 08:55:12 +0700

    init

---
+++ GitHubAction_Net_SqlServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitHubAction_Net_SqlServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
